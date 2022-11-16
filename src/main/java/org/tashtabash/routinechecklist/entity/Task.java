286feb8e8a77cd3cba3b787503381f676785fd1c
ShMP <matveyshnytkin@gmail.com>
2022-11-16 13:51:53 +0300

    Add new methods for Task

--- src/main/java/org/tashtabash/routinechecklist/entity/Task.java
+++ src/main/java/org/tashtabash/routinechecklist/entity/Task.java
@@ -2,6 +2,8 @@ package org.tashtabash.routinechecklist.entity;
 
 
 import javax.persistence.*;
+import java.util.Objects;
+
 
 @Entity
 public class Task {
@@ -20,6 +22,11 @@ public class Task {
         this.name = name;
     }
 
+    public Task(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public long getId() {
         return id;
     }
@@ -28,7 +35,29 @@ public class Task {
         return name;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        if (id != task.id) return false;
+        return Objects.equals(name, task.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
